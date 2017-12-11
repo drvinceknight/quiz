c8e50b73e537fa766c1645a77ab5b531ccce5bf8
Vince Knight <vincent.knight@gmail.com>
2017-12-11 12:46:22 +0000

    Improve formatting of questions and answers.

--- main.py
+++ main.py
@@ -3,16 +3,14 @@ Render all pdfs of the quiz.
 """
 
 import pathlib
-import subprocess
+import re
 import shutil
-
+import subprocess
 
 pwd = pathlib.Path('.')
 build_dir = pwd / "build"
 build_dir.mkdir(exist_ok=True)
 shutil.copy(str(pwd / "src/beamercolorthemesolarized.sty"), str(build_dir))
-
-
 
 for tex_file in (pwd / "src").glob("*tex"):
     tex_code = tex_file.read_text(encoding="utf8")
@@ -25,10 +23,20 @@ for tex_file in (pwd / "src").glob("*tex"):
     shutil.copy(str(build_dir) + "/" + str(out_file.stem) + ".pdf", str(pwd / "solutions"))
 
     # Write questions
-    out_file.write_text(tex_code.replace("Answer", "% Answer"), encoding="utf8")
+    tex_code = tex_file.read_text().split("\n")
+
+    # TODO All this should be a regex
+    lines = []
+    for line in tex_code:
+        if "Answer" in line:
+            line = line.replace("Answer:", "\phantom{Answer: ")
+            line = line + "}"
+        lines.append(line)
+    tex_code = "\n".join(lines)
+
+    out_file.write_text(tex_code, encoding="utf8")
     subprocess.call(["latexmk", "--xelatex", "-shell-escape", str(out_file.name)],
                     cwd = str(build_dir))
     shutil.copy(str(build_dir) + "/" + str(out_file.stem) + ".pdf", str(pwd / "questions"))
 
 shutil.rmtree(str(build_dir))
-
